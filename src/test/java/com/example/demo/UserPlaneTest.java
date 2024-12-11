a866cc7546675296b30765045ee71775cb70701d
nolan <efyjs17@nottingham.edu.my>
2024-12-11 15:13:02 +0800

    feat: userplane test case

---
+++ src/test/java/com/example/demo/UserPlaneTest.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserPlaneTest {
+	UserPlane user;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		user = new UserPlane.UserPlaneBuilder().setHealth(5).load().build();
+	}
+
+	@Test
+	void testMoveUp() {
+		double initialY = user.getLayoutY();
+		user.moveUp();
+		user.updatePosition();
+		assertTrue(initialY > (user.getLayoutY()+user.getTranslateY()));
+	}
+	
+	@Test
+	void testMoveDown() {
+		double initialY = user.getLayoutY();
+		user.moveDown();
+		user.updatePosition();
+		assertTrue(initialY < (user.getLayoutY()+user.getTranslateY()));
+	}
+	
+	@Test
+	void testMoveFront() {
+		double initialX = user.getLayoutX();
+		user.moveFront();
+		user.updatePosition();
+		assertTrue(initialX < user.getLayoutX()+user.getTranslateX());
+	}
+	
+	@Test
+	void testMoveBack() {
+		double initialX = user.getLayoutX()+16;
+		user.moveBack();
+		user.updatePosition();
+		assertTrue(initialX > user.getLayoutX()+user.getTranslateX());
+	}
+
+}
