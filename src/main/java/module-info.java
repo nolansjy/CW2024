aa7c26aabbd91f51aff2d2a105f730d673e3f54e
nolan <efyjs17@nottingham.edu.my>
2024-12-13 16:49:54 +0800

    feat: new modules added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
-
+	requires javafx.graphics;
+	requires com.fasterxml.jackson.core;
+	requires com.fasterxml.jackson.databind;
+	requires java.desktop;
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo.controller;
