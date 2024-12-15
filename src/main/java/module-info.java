ec743b1a88605d4e94db0c68f4ef434488995bf5
nolan <efyjs17@nottingham.edu.my>
2024-12-15 20:24:14 +0800

    fix: requires transitive

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module com.example.demo {	
 	requires com.fasterxml.jackson.core;
 	requires com.fasterxml.jackson.databind;
-	requires java.desktop;
+	requires transitive java.desktop;
 	requires javafx.base;
 	requires javafx.controls;
-	requires javafx.graphics;
+	requires transitive javafx.graphics;
 	
 	opens com.example.demo to javafx.fxml;
 	exports com.example.demo;
